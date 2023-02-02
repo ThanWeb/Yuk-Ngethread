083a2a58181bc85ed723e1294dcdfa7a11ba6ce0
Hans <classsickpeople@gmail.com>
2023-02-03 00:08:50 +0800

    Menambahkan action dan reducer untuk state leaderboard

---
+++ src/states/leaderboard/action.js
@@ -0,0 +1,31 @@
+import api from '../../utils/api'
+
+const ActionType = {
+    RECEIVE_LEADERBOARDS: 'RECEIVE_LEADERBOARDS'
+}
+
+const receiveLeaderboardsActionCreator = (leaderboards) => {
+    return {
+        type: ActionType.RECEIVE_LEADERBOARDS,
+        payload: {
+            leaderboards
+        }
+    }
+}
+
+const asyncReceiveLeaderboards = () => {
+    return async (dispatch) => {
+        try {
+            const leaderboards = await api.getLeaderboards()
+            dispatch(receiveLeaderboardsActionCreator(leaderboards))
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+}
+
+export {
+    ActionType,
+    receiveLeaderboardsActionCreator,
+    asyncReceiveLeaderboards
+}
