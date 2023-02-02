f7e58ca44a86b48b9d7cb5b6ddc17a5ab8d455d7
Hans <classsickpeople@gmail.com>
2023-02-03 00:02:52 +0800

    Using react router and update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
+import { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import LeaderboardPage from './pages/LeaderboardPage'
+import RegisterPage from './pages/RegisterPage'
+import DetailPage from './pages/DetailPage'
+import NotFoundPage from './pages/NotFoundPage'
+import Navigation from './components/Navigation'
+import { asyncPreloadProcess } from './states/isPreload/action'
+import { asyncUnsetAuthUser } from './states/authUser/action'
+
 const App = () => {
+    const { authUser = null, isPreload = false } = useSelector((states) => states)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(asyncPreloadProcess())
+    }, [dispatch])
+
+    const onSignOut = () => {
+        dispatch(asyncUnsetAuthUser())
+    }
+
+    if (isPreload) {
+        return null
+    }
+
+    if (authUser === null) {
+        return (
+            <div className='container'>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Routes>
+                </main>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <p>Yuk Ngethread</p>
+        <div className="container">
+            <header>
+                <Navigation authUser={authUser} signOut={onSignOut} />
+            </header>
+            <main>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/threads/:id" element={<DetailPage />} />
+                    <Route path="/leaderboard" element={<LeaderboardPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </main>
         </div>
     )
 }
