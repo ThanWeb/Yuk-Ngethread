807b95813ad69311125479c85d63e8fb6d852c21
Hans <classsickpeople@gmail.com>
2023-02-03 00:05:20 +0800

    Menambahkan fungsi getFormattedDateString

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
+const months = ['January', 'Februarry', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
 const checkEmailIsValid = (text) => {
     if (!text) {
         return false
@@ -6,4 +9,13 @@ const checkEmailIsValid = (text) => {
     return text.match(regexExp)
 }
 
-export { checkEmailIsValid }
+const getFormattedDateString = (isoDate) => {
+    const date = new Date(isoDate)
+    const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
+    const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+    const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
+
+    return `${days[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()} at ${hours}:${minutes}:${seconds}`
+}
+
+export { checkEmailIsValid, getFormattedDateString }
