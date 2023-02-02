48306ad568cc07041d90439bbabef112f330ba56
Hans <classsickpeople@gmail.com>
2023-02-03 00:05:41 +0800

    Membuat store

---
+++ src/states/index.js
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+import authUserReducer from './authUser/reducer'
+import isPreloadReducer from './isPreload/reducer'
+// import threadDetailReducer from './talkDetail/reducer'
+import threadsReducer from './threads/reducer'
+import usersReducer from './users/reducer'
+import leaderboardsReducer from './leaderboard/reducer'
+
+const store = configureStore({
+    reducer: {
+        authUser: authUserReducer,
+        isPreload: isPreloadReducer,
+        // threadDetail: threadDetailReducer,
+        threads: threadsReducer,
+        users: usersReducer,
+        leaderboards: leaderboardsReducer
+    }
+})
+
+export default store
