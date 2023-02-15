d2e031c1f596aa1ce36ca62508e5afb44919e367
Hans <classsickpeople@gmail.com>
2023-02-16 05:48:08 +0800

    Memasang package jsdom

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const vitestConfig = {
+    test: {
+        environment: 'jsdom'
+    }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    test: vitestConfig.test
 })
