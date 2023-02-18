895c5949f1cd9ecf415fbaa78c44cf3867873fd1
Hans <classsickpeople@gmail.com>
2023-02-18 18:23:34 +0800

    Membuat testing untuk PasswordInput component

---
+++ src/components/PasswordInput.test.jsx
@@ -0,0 +1,20 @@
+import { describe, expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import PasswordInput from './PasswordInput'
+import userEvent from '@testing-library/user-event'
+
+/**
+ * skenario testing
+ *
+ * - PasswordInput component
+ *   - should type the password correctly
+ */
+
+describe('PasswordInput component', () => {
+    test('should type the password correctly', async () => {
+        render(<PasswordInput props={{ placeholder: 'yourpassword' }}/>)
+        const passwordInput = await screen.getByPlaceholderText('yourpassword')
+        await userEvent.type(passwordInput, 'passwordtest')
+        expect(passwordInput.value).toContain('passwordtest')
+    })
+})
