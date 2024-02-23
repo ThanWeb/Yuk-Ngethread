96dc4655b5321f764b8cfdc96cc26fc79cbbf578
Hans <classsickpeople@gmail.com>
2024-02-23 22:57:24 +0800

    Styling not found page responsive (not logged in page)

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,14 +1,24 @@
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const NotFoundPage = () => {
-  const navigate = useNavigate()
+  const { authUser = null } = useSelector((states) => states)
 
   return (
-    <div>
-      <p>
-        <span>404</span><span>|</span><span>Not Found</span>
-      </p>
-      <button onClick={() => navigate('/')}>Back to Home</button>
+    <div className='min-w-screen min-h-screen flex bg-slate-100'>
+      <div className='m-auto flex flex-col md:flex-row md:items-center gap-y-2 gap-x-4 px-6'>
+        <img src='/favicon.png' alt='Yuk Ngethread' className='h-12 w-12'/>
+        <div className='flex flex-col gap-y-2'>
+          <p className='text-2xl font-bold'>Sorry,<br/>this page doesn&apos;t exist</p>
+          <p>
+            {
+              authUser === null
+                ? <span>May you want to <Link to={'/'} className='text-blue-800 font-medium'>Sign In</Link>?</span>
+                : <span>Back to <Link to={'/'} className='text-blue-800 font-medium'>Home</Link></span>
+            }
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
