23bf55ba71b70b1cbf3c5bc3d69e9938a2e441ce
Hans <classsickpeople@gmail.com>
2024-02-27 16:33:58 +0800

    Use try catch in api utils

--- src/utils/api.js
+++ src/utils/api.js
@@ -20,161 +20,213 @@ const api = (() => {
   }
 
   const register = async ({ name, email, password }) => {
-    const response = await fetch(`${BASE_URL}/register`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name,
-        email,
-        password
+    try {
+      const response = await fetch(`${BASE_URL}/register`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password
+        })
       })
-    })
 
-    const responseJson = await response.json()
-    return responseJson
+      const responseJson = await response.json()
+      return responseJson
+    } catch (error) {
+      return handleError(error)
+    }
   }
 
   const login = async ({ email, password }) => {
-    const response = await fetch(`${BASE_URL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        email,
-        password
+    try {
+      const response = await fetch(`${BASE_URL}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email,
+          password
+        })
       })
-    })
 
-    const responseJson = await response.json()
-    return responseJson
+      const responseJson = await response.json()
+      return responseJson
+    } catch (error) {
+      return handleError(error)
+    }
   }
 
   const getAllUsers = async () => {
-    const response = await fetch(`${BASE_URL}/users`)
-    const responseJson = await response.json()
-    const { status, message } = responseJson
+    try {
+      const response = await fetch(`${BASE_URL}/users`)
+      const responseJson = await response.json()
+      const { status, message } = responseJson
 
-    if (status !== 'success') {
-      console.log(message)
+      if (status !== 'success') {
+        console.error(message)
+      }
+
+      const { data: { users } } = responseJson
+      return users
+    } catch (error) {
+      return handleError(error)
     }
-
-    const { data: { users } } = responseJson
-    return users
   }
 
   const getOwnProfile = async () => {
-    const response = await _fetchWithAuth(`${BASE_URL}/users/me`)
-    const responseJson = await response.json()
-    const { data: { user } } = responseJson
-    return user
+    try {
+      const response = await _fetchWithAuth(`${BASE_URL}/users/me`)
+      const responseJson = await response.json()
+      const { data: { user } } = responseJson
+      return user
+    } catch (error) {
+      return handleError(error)
+    }
   }
 
   const createThread = async ({ title, body, category = '' }) => {
-    const response = await _fetchWithAuth(`${BASE_URL}/threads`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title,
-        body,
-        category
+    try {
+      const response = await _fetchWithAuth(`${BASE_URL}/threads`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          body,
+          category
+        })
       })
-    })
 
-    const responseJson = await response.json()
-    return responseJson
+      const responseJson = await response.json()
+      return responseJson
+    } catch (error) {
+      return handleError(error)
+    }
   }
 
   const getAllThreads = async () => {
-    const response = await fetch(`${BASE_URL}/threads`)
-    const responseJson = await response.json()
-    const { status, message } = responseJson
+    try {
+      const response = await fetch(`${BASE_URL}/threads`)
+      const responseJson = await response.json()
+      const { status, message } = responseJson
 
-    if (status !== 'success') {
-      console.log(message)
+      if (status !== 'success') {
+        console.error(message)
+      }
+
+      const { data: { threads } } = responseJson
+      return threads
+    } catch (error) {
+      return handleError(error)
     }
-
-    const { data: { threads } } = responseJson
-    return threads
   }
 
   const getDetailThread = async (id) => {
-    const response = await fetch(`${BASE_URL}/threads/${id}`)
-    const responseJson = await response.json()
-    return responseJson
+    try {
+      const response = await fetch(`${BASE_URL}/threads/${id}`)
+      const responseJson = await response.json()
+      return responseJson
+    } catch (error) {
+      return handleError(error)
+    }
   }
 
   const createCommentThread = async ({ content, id }) => {
-    const response = await _fetchWithAuth(`${BASE_URL}/threads/${id}/comments`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        content
+    try {
+      const response = await _fetchWithAuth(`${BASE_URL}/threads/${id}/comments`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          content
+        })
       })
-    })
 
-    const responseJson = await response.json()
-    return responseJson
+      const responseJson = await response.json()
+      return responseJson
+    } catch (error) {
+      return handleError(error)
+    }
   }
 
   const giveUpVoteThread = async (id) => {
-    const response = await _fetchWithAuth(`${BASE_URL}/threads/${id}/up-vote`, {
-      method: 'POST'
-    })
+    try {
+      const response = await _fetchWithAuth(`${BASE_URL}/threads/${id}/up-vote`, {
+        method: 'POST'
+      })
 
-    const responseJson = await response.json()
-    return responseJson
+      const responseJson = await response.json()
+      return responseJson
+    } catch (error) {
+      return handleError(error)
+    }
   }
 
   const giveDownVoteThread = async (id) => {
-    const response = await _fetchWithAuth(`${BASE_URL}/threads/${id}/down-vote`, {
-      method: 'POST'
-    })
+    try {
+      const response = await _fetchWithAuth(`${BASE_URL}/threads/${id}/down-vote`, {
+        method: 'POST'
+      })
 
-    const responseJson = await response.json()
-    return responseJson
+      const responseJson = await response.json()
+      return responseJson
+    } catch (error) {
+      return handleError(error)
+    }
   }
 
   const giveUpVoteComment = async ({ threadId, commentId }) => {
-    const response = await _fetchWithAuth(`${BASE_URL}/threads/${threadId}/comments/${commentId}/up-vote`, {
-      method: 'POST'
-    })
+    try {
+      const response = await _fetchWithAuth(`${BASE_URL}/threads/${threadId}/comments/${commentId}/up-vote`, {
+        method: 'POST'
+      })
 
-    const responseJson = await response.json()
-    return responseJson
+      const responseJson = await response.json()
+      return responseJson
+    } catch (error) {
+      return handleError(error)
+    }
   }
 
   const giveDownVoteComment = async ({ threadId, commentId }) => {
-    const response = await _fetchWithAuth(`${BASE_URL}/threads/${threadId}/comments/${commentId}/down-vote`, {
-      method: 'POST'
-    })
+    try {
+      const response = await _fetchWithAuth(`${BASE_URL}/threads/${threadId}/comments/${commentId}/down-vote`, {
+        method: 'POST'
+      })
 
-    const responseJson = await response.json()
-    return responseJson
+      const responseJson = await response.json()
+      return responseJson
+    } catch (error) {
+      return handleError(error)
+    }
   }
 
   const getLeaderboards = async () => {
-    const response = await fetch(`${BASE_URL}/leaderboards`)
-    const responseJson = await response.json()
-    const { status, message } = responseJson
+    try {
+      const response = await fetch(`${BASE_URL}/leaderboards`)
+      const responseJson = await response.json()
+      const { status, message } = responseJson
 
-    if (status !== 'success') {
-      console.log(message)
+      if (status !== 'success') {
+        console.error(message)
+      }
+
+      const { data: { leaderboards } } = responseJson
+      return leaderboards
+    } catch (error) {
+      return handleError(error)
     }
-
-    const { data: { leaderboards } } = responseJson
-    return leaderboards
   }
 
   const handleError = (error) => {
     console.error(error.message)
-    return { status: 'fail', message: 'server error' }
+    return { status: 'fail', message: 'error occured' }
   }
 
   return {
